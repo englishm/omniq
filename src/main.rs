cc87d09eee891caa2574c5453f0d229255433288
Mike English <mike.english@gmail.com>
2023-07-06 13:17:17 -0400

    Serialize example struct as JSON

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::error;
+
+#[derive(Deserialize, Serialize)]
+struct ExampleStruct {
+    children: Vec<ExampleStruct>,
+    data: Vec<u8>,
+}
+
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+fn main() -> Result<()> {
+    let example_struct = ExampleStruct {
+        children: vec![ExampleStruct {
+            children: vec![],
+            data: vec![],
+        }],
+        data: vec![1, 2, 3],
+    };
+    let output = serde_json::to_string(&example_struct)?;
+    println!("output: {output}");
+    Ok(())
 }
