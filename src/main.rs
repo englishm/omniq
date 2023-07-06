fb488b8d06a9d5d1eb35a24bb833294ccfb26707
Mike English <mike.english@gmail.com>
2023-07-06 15:01:25 -0400

    Almost correct sexpr parsing with nom

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use nom::bytes::complete::take_till;
+use nom::branch::alt;
+use nom::bytes::complete::{tag, take_till};
+use nom::multi::separated_list0;
 use nom::IResult;
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -11,14 +13,27 @@ struct ExampleStruct {
     data: Vec<u8>,
 }
 
+#[derive(Debug)]
 enum SExpr {
     List(Vec<SExpr>),
     Atom(String),
 }
 
 fn atom(input: &str) -> IResult<&str, SExpr> {
-    let (consumed, rest) = take_till(|c: char| c.is_whitespace())(input)?;
-    Ok((rest, SExpr::Atom(consumed.to_string())))
+    let (input, consumed) = take_till(|c: char| c.is_whitespace() || c == ')')(input)?;
+    Ok((input, SExpr::Atom(consumed.to_string())))
+}
+
+fn list(input: &str) -> IResult<&str, SExpr> {
+    let (input, _) = tag("(")(input)?;
+    let (input, list) = separated_list0(tag(" "), sexpr)(input)?;
+    let (input, _) = tag(")")(input)?;
+
+    Ok((input, SExpr::List(list)))
+}
+
+fn sexpr(input: &str) -> IResult<&str, SExpr> {
+    alt((list, atom))(input)
 }
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
@@ -35,7 +50,10 @@ fn main() -> Result<()> {
     let json_output = serde_json::to_string(&example_struct)?;
     println!("json_output: {json_output}");
     let sexpr_output = serde_sexpr::to_string(&example_struct)?;
-    println!("sexpr_output: {sexpr_output}");
+    println!("sexpr_output: {}", sexpr_output.clone());
+
+    let (rest, parsed_sexpr) = sexpr(sexpr_output.as_str()).unwrap();
+    dbg!(rest, parsed_sexpr);
 
     Ok(())
 }
